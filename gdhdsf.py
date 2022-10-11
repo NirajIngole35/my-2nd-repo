14425de34925e53bf5d50baf0114523688c4ec47
niraj <iniraj35@gmail.com>
2022-10-11 21:54:36 +0530

    commited

--- gdhdsf.py
+++ gdhdsf.py
@@ -1,9 +1,11 @@
 import numpy
 from numpy import ones, zeros
 x=array.array('i',[1,2,3,4,5,6])
-print(x) print(type(x)) 
+print(x) 
+print(type(x)) 
 
 #second way 
 from array import*
 x=array('i',[1,2,3,4,5,6]) 
-print(x) print(type(x))
+print(x) 
+print(type(x))
